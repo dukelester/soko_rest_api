ebb5d6b9a798d4087c4fc04e71b0f884729a2d1a
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-10-26 10:01:10 +0300

    Added a sample error handling

--- app.js
+++ app.js
@@ -25,7 +25,20 @@ app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 
 
