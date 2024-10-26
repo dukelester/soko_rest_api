fa0d29c293cd034ef934dd135bc75e59bba9236c
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-10-26 09:57:11 +0300

    Added morgan logger

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -10,6 +11,9 @@ const ordersRoutes = require('./api/routes/orders');
 //         message: "Server ready to run"
 //     });
 // });
+
+app.use(morgan('dev'));
+
 app.get('/', (req, res, next) => {
     res.status(200).json({
         message: ' Welcome to our shop REST api. We provide all e-commerce functionalities '
