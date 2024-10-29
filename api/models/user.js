ca3f52431b60a98c9fb5711dc00a89cf39459cab
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-10-29 13:37:36 +0300

    Created the usermodel

---
+++ api/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    userType: { type: String, default: 'seller' },
+});
+
+
+module.exports = mongoose.model("User", UserSchema);
