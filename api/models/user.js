239ef9da7ba31252f7dd19116ddc3b52a5bc7821
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-10-29 16:11:47 +0300

    Added the email validation regex

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     fullName: { type: String, required: true },
-    email: { type: String, required: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)+/
+    },
     password: { type: String, required: true },
     username: { type: String, required: true },
     phoneNumber: { type: String, required: true },
